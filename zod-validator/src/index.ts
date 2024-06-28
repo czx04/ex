a7f318b1d10ca01e36986e8ca5cad9d647dba4a5
czx04 <dhit2k4@gmail.com>
2024-06-29 02:35:48 +0700

    update zod p8

--- zod-validator/src/index.ts
+++ zod-validator/src/index.ts
@@ -16,7 +16,7 @@ type User = {
 
 const userSchema = z.object({
     username: z.string().min(1, { message: 'must be at least 1 character' }).max(50),
-    email: z.string().email({ message: 'Invalid email address' }).min(1).max(50),
+    email: z.string().email({ message: 'invalid email address' }).min(1).max(50),
     password: z.string()
         .min(3, { message: 'Password must be at least 3 characters long' })
         .max(50, { message: 'Password must be at most 50 characters long' })
@@ -24,23 +24,24 @@ const userSchema = z.object({
         .regex(/[A-Z]/, { message: 'Password must contain at least 1 uppercase letter' })
         .regex(/\d/, { message: 'Password must contain at least 1 number' })
         .regex(/[^a-zA-Z0-9]/, { message: 'Password must contain at least 1 special character' }),
-    age: z.number().refine((age)=> age > 18, { message: 'must be > 18 age'}),
+    age: z.number().gte(5).refine((age)=> age < 18, { message: 'must be > 18 age'}),
+    // gte : min  lte : max 
     testnumber: z.number().superRefine((val, ctx) => {
         if (val < 10) {
           ctx.addIssue({
             code: z.ZodIssueCode.custom,
             message: "should be >= 10",
             fatal: true,
-          });
+          })
       
-          return z.NEVER;
+          return z.NEVER
         }
       
         if (val !== 12) {
           ctx.addIssue({
             code: z.ZodIssueCode.custom,
             message: "should be twelve",
-          });
+          })
         }
       })
 })
@@ -49,7 +50,7 @@ const user: User = {
     username: 'mn',
     email: 'hungdu@gmail.com',
     password: 'aabA1!',
-    age : 16,
+    age : 9,
     testnumber: 9
 }
 
@@ -58,6 +59,22 @@ try {
 } catch (e : any) {
     console.error(e.errors)
 }
+
+
+const stringPromise = z.string().promise() // Tạo schema cho Promise<string>
+
+const myPromise: Promise<string> = Promise.resolve("djt con me met vcll")
+
+// xác thực myPromise với schema
+stringPromise.parse(myPromise)
+  .then(validatedValue => {
+    console.log("Validated value:", validatedValue)
+    // if promise có giá trị là string thì in ro string k thì .catch ra lỗi 
+  })
+  .catch(error => {
+    console.error("Validation failed:", error);
+  })
+
 
 app.listen(PORT, () => {
     console.log(`App running on port: ${PORT}`)
